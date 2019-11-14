656a8526eb1b96a5aa779b98191a5b050f094f4b
Patrick MacMurchy <pmacmurchy@pairin.com>
2019-11-14 08:54:56 -0700

    Remove the animate loop because it's not necessary for this one

--- bezier-curve/canvas.js
+++ bezier-curve/canvas.js
@@ -2,6 +2,8 @@ canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight * .8;
+const codeBody = document.getElementById('bezierCurveCode');
+
 
 class Circle {
   constructor(x, y, radius = 10, color) {
@@ -74,6 +76,7 @@ canvas.addEventListener('mousemove', (e) => {
   if(mouse.draggingCircle) {
     mouse.draggingCircle.update(mouse)
     mouse.draggingCircle.draw(ctx)
+    process()
   }
 });
 canvas.addEventListener('mousedown', dragStart);
@@ -112,24 +115,17 @@ const process = () => {
   drawCurve();
   connectControlPointsToLine(linePoints.cp1, linePoints.origin);
   connectControlPointsToLine(linePoints.cp2, linePoints.destination);
+  codeBody.innerHTML = codeOutput();
 }
 
-const gameLoop = () => {
-  let lastRender = null;
-  const animate = (timestamp) => {
-    const delta = lastRender ? timestamp - lastRender : 32;
-    const renders = Math.floor(delta /  (1000/60)); // ms passed / frames per second
-    ctx.fillStyle = '#000';
-    if(renders > 0) {
-      for(let i = 0; i < renders; i++) {
-        process()
-        lastRender = timestamp;
-      }
-    }
 
-    window.requestAnimationFrame(animate);
-  }
-  window.requestAnimationFrame(animate)
-}
+const codeOutput = () => (`
+  ctx.lineWidth = 6;
+  ctx.strokeStyle = "#000";
+  ctx.beginPath();
+  ctx.moveTo(${linePoints.origin.x}, ${linePoints.origin.y});
+  ctx.bezierCurveTo(${linePoints.cp1.x}, ${linePoints.cp1.y}, ${linePoints.cp2.x}, ${linePoints.cp2.y}, ${linePoints.destination.x}, ${linePoints.destination.y});
+  ctx.stroke();
+`)
 
-gameLoop();
+process();
